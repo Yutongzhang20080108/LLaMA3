e6c5e7606dbedfe1c688d5e95a890010cb8f1a8d
Aaron Zhang <yutongzhang20080108@outlook.com>
2024-08-21 20:14:50 +0800

    Updata model.py and README

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 I'm going to implement LLaMA3-8B in the repo
+Goals:
+1. Grouped Query Attention
+2. RMSNorm
+3. training bug
+4. RoPE
--- model.py
+++ model.py
@@ -5,8 +5,9 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 from dataclasses import dataclass
-from transformers import GPT2Tokenizer
+import tiktoken
 import numpy as np
+import time
 #---------------------------------------------------------
 
 #Part1: Config
@@ -25,7 +26,7 @@ class LLaMAConfig:
 
 #Part2: Tokenizer
 #We are going to use the tokenizer from Huggingface.
-tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+tokenizer = tiktoken.get_encoding("gpt2")
 
 #Part3: DataLoader
 #Function: 1. get one tokenized batch of data
@@ -40,9 +41,9 @@ class DataLoader:
     
     def get_batch(self):
         batch = self.data[self.current_position: self.current_position + self.batch_size*self.max_seq_len]
-        batch = torch.tensor(batch).view(self.batch_size, self.max_seq_len)
+        batch = torch.tensor(batch).view(self.batch_size, self.max_seq_len).to(device)
         target = self.data[self.current_position + 1: self.current_position + self.batch_size*self.max_seq_len+1]
-        target = torch.tensor(target).view(self.batch_size, self.max_seq_len)
+        target = torch.tensor(target).view(self.batch_size, self.max_seq_len).to(device)
         self.current_position += self.batch_size*self.max_seq_len
         if self.current_position + self.batch_size*self.max_seq_len > len(self.data):
             self.current_position = 0
@@ -60,14 +61,24 @@ class SelfAttention(nn.Module):
         self.head_dim = n_embd // n_head
 
     def forward(self, x):
-        B, T, C = x.size()
-        q,k,v = self.qkv(x).chunk(3, dim=-1)
-        q = q.view([B, T, self.n_head, C // self.n_head]).transpose(1, 2)
-        k = k.view([B, T, self.n_head, C // self.n_head]).transpose(1, 2)
-        v = v.view([B, T, self.n_head, C // self.n_head]).transpose(1, 2)
-        y = F.scaled_dot_product_attention(q, k, v, is_causal=True)
-        y = y.transpose(1, 2).view([B, T, C])
-        return y
+        if x.dim() == 2:
+            T, C = x.size()
+            q, k, v = self.qkv(x).chunk(3, dim=-1)
+            q = q.view([T, self.n_head, C // self.n_head]).transpose(0, 1)
+            k = k.view([T, self.n_head, C // self.n_head]).transpose(0, 1)
+            v = v.view([T, self.n_head, C // self.n_head]).transpose(0, 1)
+            y = F.scaled_dot_product_attention(q, k, v, is_causal=True)
+            y = y.transpose(0, 1).contiguous().view([T, C])
+            return y
+        if x.dim() == 3:
+            B, T, C = x.size()
+            q,k,v = self.qkv(x).chunk(3, dim=-1)
+            q = q.view([B, T, self.n_head, C // self.n_head]).transpose(1, 2)
+            k = k.view([B, T, self.n_head, C // self.n_head]).transpose(1, 2)
+            v = v.view([B, T, self.n_head, C // self.n_head]).transpose(1, 2)
+            y = F.scaled_dot_product_attention(q, k, v, is_causal=True)
+            y = y.transpose(1, 2).contiguous().view([B, T, C])
+            return y
 
 #We are going to implemet the activation function used in LLaMA model.
 #The equation looks like this SwiGLU(x) = x * sigmoid(beta * x) + (1 - sigmoid(beta * x)) * (Wx + b)
@@ -96,14 +107,14 @@ class MLP(nn.Module):
 class DecoderBlock(nn.Module):
     def __init__(self, config):
         super().__init__()
-        self.RMSnorm = nn.RMSNorm(config.n_embd)
+        self.LayerNorm1 = nn.LayerNorm(config.n_embd)
         self.SelfAttention = SelfAttention(config.n_embd, config.n_head)
-        self.RMSnorm = nn.RMSNorm(config.n_embd)
+        self.LayerNorm2 = nn.LayerNorm(config.n_embd)
         self.mlp = MLP(config.n_embd, config.mlp_ratio)
 
     def forward(self, x):
-        x = x + self.SelfAttention(self.RMSnorm(x))
-        x = x + self.mlp(self.RMSnorm(x))
+        x = x + self.SelfAttention(self.LayerNorm1(x))
+        x = x + self.mlp(self.LayerNorm2(x))
         return x
 
 class LLaMA3(nn.Module):
@@ -115,7 +126,7 @@ class LLaMA3(nn.Module):
         self.Decoder = nn.ModuleList([DecoderBlock(config) for _ in range(config.n_layer)])
         self.ln = nn.Linear(config.n_embd, config.n_vocab)
         self.loss_fn  = nn.CrossEntropyLoss()
-        self.RMSnorm = nn.RMSNorm(config.n_embd)
+        self.LayerNorm = nn.LayerNorm(config.n_embd)
     
     #forward function is basically the training loop
     def forward(self, idx, target=None):
@@ -128,7 +139,7 @@ class LLaMA3(nn.Module):
         emb = token_emb + pos_emb
         for block in self.Decoder:
             emb = block(emb)
-        logits = self.ln(self.RMSnorm(emb))
+        logits = self.ln(self.LayerNorm(emb))
         if target is not None:
             loss = self.loss_fn(logits.view(-1, self.config.n_vocab), target.view(-1))
             return logits, loss
@@ -138,36 +149,38 @@ class LLaMA3(nn.Module):
     #The generation is actually not the same as QA but the text completion
     def generation(self, idx, num_return_sequences=1, max_tokens=64):
         model.eval()
-        tokenized_sentence = torch.tensor(tokenizer.encode(idx))
-        num_tokenized_sentence = tokenized_sentence.repeat(num_return_sequences, 1).contiguous()
-        num_tokenized_sentence = num_tokenized_sentence.to(device)
+        tokenized_sentence = torch.tensor(tokenizer.encode(idx)).to(device)
         if num_return_sequences == 1:
-            T = tokenized_sentence.size(1)
+            T = tokenized_sentence.size(-1)
             assert T <= self.config.max_seq_len
             for i in range(max_tokens-T):
-                T = tokenized_sentence.size(1)
+                T = tokenized_sentence.size(-1)
                 pos = torch.arange(0, T, dtype=torch.long, device=device)
-                token_emb = self.emb(num_tokenized_sentence)
+                token_emb = self.emb(tokenized_sentence)
                 pos_emb = self.pos_emb(pos)
                 emb = token_emb + pos_emb
                 for block in self.Decoder:
                     emb = block(emb)
-                logits = self.ln(emb)
+                logits = self.ln(self.LayerNorm(emb))
+                logits = logits[-1, :]
+                next_tokens_probs = F.softmax(logits, dim=-1)
+                next_token = torch.multinomial(next_tokens_probs, 1)
+                tokenized_sentence = torch.cat([tokenized_sentence, next_token], dim=-1)
+
+            generation = tokenizer.decode(tokenized_sentence.tolist())
+            return generation
+        else:
+            num_tokenized_sentence = tokenized_sentence.repeat(num_return_sequences, 1).contiguous()
+            B, T = num_tokenized_sentence.size()
+            for i in range(max_tokens-T):
+                B, T = num_tokenized_sentence.size()   
+                logits = self.forward(num_tokenized_sentence)
                 logits = logits[:, -1, :]
                 next_tokens_probs = F.softmax(logits, dim=-1)
                 next_token = torch.multinomial(next_tokens_probs, 1)
-                tokenized_sentence = torch.cat([num_tokenized_sentence, next_token], dim=-1)
-
-            generation = tokenizer.decode(idx[0].tolist())
-            return generation
-        else:
-            B, T = num_tokenized_sentence.size()   
-            logits = self.forward(idx)
-            logits = logits[:, -1, :]
-            next_tokens_probs = F.softmax(logits, dim=-1)
-            next_token = torch.multinomial(next_tokens_probs, 1)
-            idx = torch.cat([idx, next_token], dim=-1)
-            generation = [tokenizer.decode(idx[i].tolist()) for i in range(B)]
+                num_tokenized_sentence = torch.cat([num_tokenized_sentence, next_token], dim=-1)
+            
+            generation = [tokenizer.decode(num_tokenized_sentence[i].tolist()) for i in range(B)]
             return generation
         
 
@@ -189,6 +202,7 @@ class lr:
 def Pretrainer(model, dataloader, config):
     model.train()
     for i in range(config.total_steps):
+        t0 = time.time()
         x, y = dataloader.get_batch()
         lr_generator = lr(config)
         lr_current = lr_generator.get_lr(i)
@@ -196,8 +210,11 @@ def Pretrainer(model, dataloader, config):
         logits, loss = model.forward(x, y)
         loss.backward()
         optimizer.step()
+        t1 = time.time()
+        duration = t1 - t0
+        tokens_per_sec = config.max_seq_len*config.batch_size / duration
         if i % 100 == 0:
-            print(f"Step: {i}, Loss: {loss.item()}, LR: {lr_current}")
+            print(f"Step: {i}, Loss: {loss.item()}, LR: {lr_current:.3f}, Token/s: {tokens_per_sec:.3f}, Time: {duration*1000:.3f}ms")
 #---------------------------------------------------------
 #Part6: Test
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -208,17 +225,16 @@ with open("input.txt", "r") as f:
     data = f.read()
 dataloader = DataLoader(data, LLaMAConfig())
 model = LLaMA3(LLaMAConfig()).to(device)
-model = torch.compile(model)
 #x, y = dataloader.get_batch()
 #print(x.shape, y.shape)
 #logits, loss = model(x, y)
 #print(logits.shape, loss)
 
-#test sampling
+"""#test sampling
 source_sentence = "Hello world"
-generation = model.generation(source_sentence, 1)
-print(generation)
+generation = model.generation(source_sentence, 2)
+print(generation)"""
 
-#Pretrainer(model, dataloader, LLaMAConfig())
+Pretrainer(model, dataloader, LLaMAConfig())
 
 
